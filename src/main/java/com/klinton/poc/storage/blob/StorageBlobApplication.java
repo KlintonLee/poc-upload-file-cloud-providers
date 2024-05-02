f62a44b96cc9d8661055793f1bb3464feb36d254
klinton lee <klintonlee92@gmail.com>
2024-05-02 02:54:56 -0300

    chore: initial structure

---
+++ src/main/java/com/klinton/poc/storage/blob/StorageBlobApplication.java
@@ -0,0 +1,13 @@
+package com.klinton.poc.storage.blob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorageBlobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorageBlobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/klinton/poc/storage/blob/StorageBlobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klinton.poc.storage.blob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorageBlobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
